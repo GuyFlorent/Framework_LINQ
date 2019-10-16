70d046d0a570311a65755758b0d50e3d8a811aca
stagiaire <stagiaire@PC-B-6-95R9J32>
2019-10-16 11:39:30 +0200

    ajout des autres requetes style Sql et c'est poussible de mettre plusieur where

--- Program.cs
+++ Program.cs
@@ -37,8 +37,48 @@ namespace testLinq
             foreach (var row in filterResult2)
             {
 
-                Console.WriteLine(row);
+                //Console.WriteLine(row);
             }
+
+            /*Quel est le nombre d’éléments qui sont des multiples de 3 ?
+Quelle est la somme des élements  supérieurs à 20 ?
+Quelles est la moyenne des multiples de 3 ?
+Inverser l’ordre des données de 2 manières différentes 
+Quelle est la valeur minimale de la liste ?*/
+
+            var MultiTrois = list.Where(f => f %3 == 0).Count();
+            Console.WriteLine(MultiTrois);
+
+
+            var Sum = list.Where(f => f >20).Sum();
+           
+
+                Console.WriteLine(Sum);
+
+            var Moyenne = list.Where(f => f % 3 == 0).Average();
+            Console.WriteLine(Moyenne);
+
+
+            var OrdreASC = list.OrderBy(f => f);
+
+            foreach (var row in OrdreASC)
+            {
+
+             //   Console.WriteLine(row);
+            }
+
+            var OrdreDESC = list.OrderByDescending(f => f);
+
+            foreach (var row in OrdreDESC)
+            {
+
+               // Console.WriteLine(row);
+            }
+
+
+            var valeurMin = list.Min(f => f);
+
+           // Console.WriteLine(valeurMin);
         }
     }
 }
